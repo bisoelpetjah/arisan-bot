0e6c321ccaaf6e240b9902a6d303456fcb6e2761
Irvan Adhitya <irvan@styletheory.co>
2018-05-02 03:21:31 -0700

    add port env var

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ docker rmi -f arisan-bot
 npm i
 npm run build
 docker build -t arisan-bot .
-docker run -d -p 4000:4000 --name arisan-bot --restart unless-stopped -e FB_ACCESS_TOKEN=a -e FB_VERIFY_TOKEN=a -e FB_APP_SECRET=a arisan-bot
+docker run -d -p 4000:4000 --name arisan-bot --restart unless-stopped -e PORT:4000 -e FB_ACCESS_TOKEN=a -e FB_VERIFY_TOKEN=a -e FB_APP_SECRET=a arisan-bot
 npm run cleanup
