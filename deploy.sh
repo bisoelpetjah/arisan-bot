741399c7448b1b793c8700ba7b368e6857a34e10
Irvan Adhitya <irvan@styletheory.co>
2018-05-02 03:16:17 -0700

    add build step

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 docker rm -f arisan-bot
 docker rmi -f arisan-bot
+npm i
 npm run build
 docker build -t arisan-bot .
 docker run -d -p 4000:4000 --name arisan-bot --restart unless-stopped -e FB_ACCESS_TOKEN=a -e FB_VERIFY_TOKEN=a -e FB_APP_SECRET=a arisan-bot
